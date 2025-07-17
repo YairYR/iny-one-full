eb00982d8a9df4683b21abbd2bc8bfd21a3ba96a
Yair <154460718+YairYR@users.noreply.github.com>
2025-07-17 04:03:44 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -304,7 +304,7 @@ export default function Home() {
       <footer className="bg-white py-6 text-center text-sm text-gray-500">
         {texts[lang].footer}{' '}
         <a
-          href="https://www.linkedin.com/in/yair-yuhaniak"
+          href="https:/www.linkedin.com/in/yairyuhaniak"
           target="_blank"
           rel="noopener noreferrer"
           className="text-indigo-600 hover:text-indigo-800 font-medium"
