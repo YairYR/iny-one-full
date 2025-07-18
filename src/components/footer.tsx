0d734e8040912484410c0561e351595d0f56719d
Yair <154460718+YairYR@users.noreply.github.com>
2025-07-17 21:51:25 -0400

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,15 +5,7 @@ export default function Footer() {
 
     return (
         <footer className="bg-white py-6 text-center text-sm text-gray-500">
-            {lang.get('footer')}{' '}
-            <a
-            href="https://www.linkedin.com/in/yairyuhaniak"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-indigo-600 hover:text-indigo-800 font-medium"
-            >
-            {lang.get('linkedin')}
-            </a>
+            {lang.get('footer')}
         </footer>
     )
 }
