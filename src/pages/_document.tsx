24626a090e4f8fde49cb7e45266ab967352cdc06
Yair <154460718+YairYR@users.noreply.github.com>
2025-07-24 00:16:05 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,17 @@ export default function Document() {
       <Head>
         {/* Meta tag para verificación de Google Search Console */}
         <meta name="google-site-verification" content="WNueup03P4lmVxxos0qDu1zwMrCeEpuS4FVUuS0XHtM" />
+
+        {/* Google Ads Global Site Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-963256646"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-963256646');
+          `
+        }} />
       </Head>
       <body>
         <Main />
