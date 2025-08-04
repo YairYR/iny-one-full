cfded399dfce95131a8d94dfb98d34869fccdf20
phgac <baladaphilippe@gmail.com>
2025-08-04 12:22:40 -0400

    - remove code localStorage from hook to utils folder

---
+++ src/utils/localstorage.ts
@@ -0,0 +1,32 @@
+interface LocalData {
+  [key: string]: {
+    value: string,
+    expires: number
+  }
+}
+
+const getAll = (): LocalData => {
+  if(typeof window === 'undefined' || !window.localStorage) return {};
+  const json = window.localStorage.getItem('data');
+  if(!json) return {};
+  return JSON.parse(json);
+}
+
+export const addToLocalStorage = (name: string, value: string, expiresInSec: number) => {
+  const data = getAll();
+  data[name] = {
+    value,
+    expires: Date.now() + (expiresInSec * 1000),
+  };
+  window.localStorage.setItem('data', JSON.stringify(data));
+}
+
+export const getFromLocalStorage = (name: string) => {
+  const data = getAll();
+  if(!data[name]) return null;
+  const value = data[name];
+  const expiresIn = (new Date(value.expires)).getTime();
+  if((Date.now() - expiresIn) <= 0) return null;
+  return value.value;
+}
+
