ce31d1f50640ec6911c68ed5f1ad30512a735776
phgac <baladaphilippe@gmail.com>
2025-08-07 16:31:54 -0400

    - ALLOWED_ORIGINS now supports preview deployments

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,11 @@
-export const IS_PRODUCTION: boolean = process.env.NODE_ENV === 'production';
+export const IS_PRODUCTION: boolean = process.env.VERCEL_ENV === 'production';
+
+export const ALLOWED_ORIGINS = ['https://iny.one', 'https://www.iny.one'];
+
+if(process.env.VERCEL_URL) {
+  ALLOWED_ORIGINS.push(`https://${process.env.VERCEL_URL}`);
+}
+
+if(!IS_PRODUCTION) {
+  ALLOWED_ORIGINS.push(`http://localhost:3000`);
+}
