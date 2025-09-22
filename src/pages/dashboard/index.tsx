62deccc3a26fa10f48c0bc8311a89aec9aad8c76
phgac <baladaphilippe@gmail.com>
2025-09-22 19:48:46 -0300

    created a reusable hook table "useDataTable"

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,36 +1,70 @@
 import type { GetServerSidePropsContext } from 'next'
+import dynamic from 'next/dynamic';
 
 import LayoutMain from "@/components/layouts/LayoutMain";
 import { AppProvider, useAppContext } from "@/contexts/app.context";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { UserClient } from "@/lib/types";
 import { useRouter } from "next/router";
-import DataTable from "@/components/Table/DataTable";
 import { Column } from "@/components/Table/hooks/useDataTable";
+import { DataTableProps } from "@/components/Table/DataTable";
+import { SkeletonTable } from "@/components/Skeleton/Skeleton";
+import { Transition } from "@headlessui/react";
+
+const DataTable = dynamic<DataTableProps<UserUrl>>(() => import('@/components/Table/DataTable'), {
+  ssr: false,
+  loading: () => <SkeletonTable rows={9} />,
+});
 
 function DashboardPage({ urls }: { urls: UserUrl[] }) {
   const { user } = useAppContext();
   const router = useRouter();
   const locale = router.locale ?? 'en';
 
+  const [data, setData] = React.useState<UserUrl[]>(urls);
+
   const columns: Column<UserUrl>[] = useMemo(() => [
     // { key: "title", label: "TÍTULO" },
-    { key: "destination", label: "URL",
+    {
+      key: "destination",
+      label: "URL",
       cellClassName: "text-indigo-600 font-medium truncate max-w-[200px]",
-      render: row => <span title={row.destination}>{row.destination}</span> },
+      render: row => <span title={row.destination}>{row.destination}</span>,
+    },
     // { key: "clicks", label: "CLICKS", sortable: true, render: (row) => (
     //     <Tag color="bg-green-100 text-green-700">{row.clicks}</Tag>) },
-    { key: "utm_source", label: "SOURCE",
+    {
+      key: "utm_source",
+      label: "SOURCE",
       sortable: true,
-      render: (row) => <Tag color="bg-indigo-100 text-indigo-700">{row.utm_source}</Tag> },
-    { key: "utm_medium", label: "MEDIUM",
+      render: (row) => {
+        // console.log(cell);
+        return <Tag color="bg-indigo-100 text-indigo-700">{row.utm_source}</Tag>;
+      },
+    },
+    {
+      key: "utm_medium",
+      label: "MEDIUM",
       sortable: true,
-      render: (row) => <Tag color="bg-purple-100 text-purple-700">{row.utm_medium}</Tag> },
-    { key: "utm_campaign", label: "CAMPAIGN",
-      sortable: true, render: (row) => (
-        <Tag color="bg-orange-100 text-orange-700">{row.utm_campaign}</Tag>) },
-    { key: "created_at", label: "FECHA", sortable: true,
-      render: (row) => (new Date(row.created_at)).toLocaleDateString(locale, {}) },
+      render: (row) => <Tag color="bg-purple-100 text-purple-700">{row.utm_medium}</Tag>,
+    },
+    {
+      key: "utm_campaign",
+      label: "CAMPAIGN",
+      sortable: true,
+      render: (row) => <Tag color="bg-orange-100 text-orange-700">{row.utm_campaign}</Tag>,
+    },
+    {
+      key: "created_at",
+      label: "FECHA",
+      sortable: true,
+      render: (row) => (new Date(row.created_at)).toLocaleDateString(locale),
+    },
+    {
+      key: 'uwu',
+      label: 'uwu',
+      render: (row) => (<>Holis</>)
+    }
   ], [locale]);
 
   const sliceText = (text: string, max: number) => {
@@ -49,12 +83,31 @@ function DashboardPage({ urls }: { urls: UserUrl[] }) {
     }
   }
 
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    // Trigger the component to show after a delay or based on user interaction
+    setTimeout(() => setShowComponent(true), 400);
+  }, []);
+
   return (
     <LayoutMain>
       <h1>Hello, {user?.email || 'user'}!</h1>
 
       <div className="p-6">
-        <DataTable<UserUrl> data={urls} columns={columns} pageSize={5} selectable={true} />
+        <Transition show={!showComponent}>
+          <div className="relative">
+            <div className="w-full transition duration-300 ease-in data-closed:opacity-0">
+              <SkeletonTable rows={10} />
+            </div>
+          </div>
+        </Transition>
+        {showComponent && (<DataTable
+          data={data}
+          columns={columns}
+          // pageSize={5}
+          selectable={true}
+        />)}
       </div>
     </LayoutMain>
   )
