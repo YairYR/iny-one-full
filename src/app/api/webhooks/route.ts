9507cf6f2d63b2054536a61abcd6e61a0f67fae8
phgac <baladaphilippe@gmail.com>
2025-11-02 17:49:25 -0300

    paypal webhook tests

---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const body = request.json();
+
+  console.log('============ webhook ============');
+  console.log('--- HEADERS ---');
+  console.log(request.headers);
+  console.log('--- BODY ---');
+  console.log(body);
+
+  return NextResponse.json({});
+}
