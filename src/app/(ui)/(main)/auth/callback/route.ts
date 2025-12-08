ee91b48601878b505aaa222b6df0ef7959380857
phgac <baladaphilippe@gmail.com>
2025-12-08 17:14:11 -0300

    fix login oauth google

--- src/app/(ui)/(main)/auth/callback/route.ts
+++ src/app/(ui)/(main)/auth/callback/route.ts
@@ -13,8 +13,9 @@ export async function GET(request: NextRequest) {
   }
 
   if (code) {
-    const supabase = await createClient()
+    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code)
+    console.log(error);
     if (!error) {
       const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'development'
