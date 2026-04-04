b56bcf46f3728f365a5b1aaf190a86f448400eec
Yair <154460718+YairYR@users.noreply.github.com>
2026-04-04 01:37:49 -0300

    Refactor ROUTES to remove BASE_PATH dependency

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,17 +1,14 @@
-export const BASE_PATH = '/ui';
-
 export const ROUTES = {
-  HOME: `${BASE_PATH}`,
-  DASHBOARD: `${BASE_PATH}/dashboard`,
-  // TEAMS: `${BASE_PATH}/teams`,
-  CART: `${BASE_PATH}/cart`,
-  ABOUT: `${BASE_PATH}/about`,
-  CHECKOUT: `${BASE_PATH}/cart/checkout`,
-  LOGIN: `${BASE_PATH}/auth/login`,
-  REGISTER: `${BASE_PATH}/auth/register`,
-  LOGOUT: `${BASE_PATH}/auth/logout`,
-  PLANS: `${BASE_PATH}/plans`,
-  PISCOLAS: `${BASE_PATH}/piscolas`,
+  HOME: '/',
+  DASHBOARD: '/dashboard',
+  CART: '/cart',
+  ABOUT: '/about',
+  CHECKOUT: '/cart/checkout',
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/register',
+  LOGOUT: '/auth/logout',
+  PLANS: '/plans',
+  PISCOLAS: '/piscolas',
 } as const;
 
 export const ALL_ROUTES: string[] = Object.values(ROUTES);
