582af69680c2357eee92779b7d7a972fb2ed8fe8
Yair <154460718+YairYR@users.noreply.github.com>
2026-04-04 00:11:03 -0300

    Update sitemap URLs and change frequencies

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,33 +1,26 @@
 import { type MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://iny.one';
+  const baseUrl = 'https://www.iny.one';
 
   return [
     {
-      url: baseUrl,
-      changeFrequency: 'monthly',
-      priority: 1
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
     },
     {
-      url: baseUrl + '/ui',
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 1
+      priority: 0.8,
     },
     {
-      url: baseUrl + '/about',
+      url: `${baseUrl}/piscolas`,
+      lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8
+      priority: 0.9,
     },
-    {
-      url: baseUrl + '/ui/about',
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: baseUrl + '/ui/piscolas',
-      changeFrequency: 'monthly',
-      priority: 0.9
-    }
   ];
 }
