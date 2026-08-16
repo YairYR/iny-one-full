0184069c9de3e001a5866c97f1a5d6ef290f89e4
phgac <baladaphilippe@gmail.com>
2026-08-16 19:28:02 -0400

    new payment flow

--- src/components/PricingCards/PricingCard.tsx
+++ src/components/PricingCards/PricingCard.tsx
@@ -51,6 +51,21 @@ export default function PricingCard({ plan, onClick }: Readonly<IPricingCard>) {
 
   const onApprove = async (data: OnApproveDataSubscriptions) => {
     console.log("onApprove", data);
+
+    const resp: SuccessResponse<{ subscriptionId: string }>|ErrorResponse = await fetch('/api/v1/subscription/capture', {
+      method: "PATCH",
+      body: JSON.stringify({
+        id: data.subscriptionId,
+      })
+    }).then((res) => res.json());
+
+    // TODO: validar respuesta y sesión
+    if (resp.ok) {
+      alert("Subscription was successfully approved!");
+      return;
+    }
+
+    alert("Subscription was not approved! Try again!");
   }
 
   const onCancel = async (data: OnCancelDataOneTimePayments) => {
@@ -102,7 +117,8 @@ export default function PricingCard({ plan, onClick }: Readonly<IPricingCard>) {
 
       {plan.id && typeof plan.id === 'string' && (
           <PayPalSubscriptionButton
-              createSubscription={() => createSubscription(plan.id!) as any}
+              // @ts-expect-error Create subscription or redirect to log in
+              createSubscription={() => createSubscription(plan.id!)}
               onApprove={onApprove}
               onCancel={onCancel}
               onError={onError}
